ad20f653c67e027186ae4cd7efa2ceb16d8f5f50
Edwin <edwinvrooij@gmail.com>
2017-03-10 14:32:49 +0100

    pretty fancy one right here

--- main.c
+++ main.c
@@ -1,12 +1,37 @@
 #include <stdio.h>
+#include <memory.h>
+
+// Declare structures
+struct Student;
+
+// Declare functions
+void printStudent(struct Student);
+
+// Initialize structures
+struct Student {
+    char name[100];
+    int age;
+    int studentNumber;
+};
 
 int main() {
+    struct Student student1;
+    struct Student student2;
 
-    int i, j, k;
+    // Student 1 specs
+    strcpy(student1.name, "Naam");
+    student1.age = 18;
+    student1.studentNumber = 10040;
 
-    i = 10;
+    // Student 2 specs
+    strcpy(student2.name, "Naam2");
+    student2.age = 19;
+    student2.studentNumber = 10041;
 
-    printf("I is %d", i);
+    printStudent(student1);
+    printStudent(student2);
+}
 
-    return 0;
+void printStudent(struct Student student) {
+    printf("Student specs: %s %d %d\n", student.name, student.age, student.studentNumber);
 }
