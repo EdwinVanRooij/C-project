c5b7a5ae781c23c79badb96701892ef968f4639e
Edwin <edwinvrooij@gmail.com>
2017-03-13 10:58:42 +0100

    Completed highly inefficient solution to project euler problem 3, optimizations may be performed later on

--- main.c
+++ main.c
@@ -1,30 +1,57 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <math.h>
+#include <time.h>
+
+bool isDivisible(long, int);
 
 bool isPrime(int);
 
-int getLargestPrimeFactor(long);
-
 int main() {
-    long number = 600851475143;
+    // Start a clock
+    clock_t begin = clock();
 
-    int result = getLargestPrimeFactor(number);
+    int result;
 
-    printf("Result: %d", result);
-}
+//    long remainder = 600851475143;
+    long starting_number = 244389;
+    long remainder = starting_number;
+    int highest_factor;
 
-int getLargestPrimeFactor(long sum) {
-    long highestFactor = 0;
-    int x = 0;
-    float tmp = 0;
-    while (x < sum) {
-        tmp = sum / x;
-        if (isPrime(x) && tmp % 1.0 == 0) {
-            highestFactor = tmp;
+    int x = 2;
+
+    // Loop while remainder is higher than 1
+    while (remainder > 1) {
+        // Make sure we have a prime number
+        if (!isPrime(x)) {
+            x++;
+            continue;
+        }
+
+        // If remainder is divisible by x, set new highest factor
+        if (isDivisible(remainder, x)) {
+            highest_factor = x;
+            // Set new remainder
+            remainder = remainder / x;
+            x = 2;
         }
         x++;
     }
-    return (int) highestFactor;
+    clock_t end = clock();
+    double time_spent = (double) (end - begin) / CLOCKS_PER_SEC;
+
+    printf("Found result '%d' for number %ld in %f seconds.\n", highest_factor, starting_number, time_spent);
+}
+
+bool isDivisible(long numberToDivide, int numberToDivideWith) {
+    float x = numberToDivide;
+    float y = numberToDivideWith;
+    float z = x / y;
+
+    if (floor(z) == z) {
+        return true;
+    }
+    return false;
 }
 
 bool isPrime(int number) {
