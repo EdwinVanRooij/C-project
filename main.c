704409dbc4cdd9d1a290e21460698765b35a6f83
Edwin <edwinvrooij@gmail.com>
2017-03-14 12:06:46 +0100

    fixed data types, really understanding this is another thing though.

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 #include <memory.h>
 
-int startProgramForAnswer();
+long startProgramForAnswer();
 
 bool isDivisible(long, int);
 
@@ -13,54 +13,48 @@ int main(int argc, char *argv[]) {
     // Start a clock
     clock_t begin = clock();
 
-    int answer = startProgramForAnswer();
+    long answer = startProgramForAnswer();
 
     clock_t end = clock();
     double time_spent = (double) (end - begin) / CLOCKS_PER_SEC;
 
-    printf("Found answer '%d' in %f seconds.\n", answer, time_spent);
+    printf("Found answer '%ld' in %f seconds.\n", answer, time_spent);
 }
 
 /**
  * Used to solve the euler problem.
  * @return the answer
  */
-int startProgramForAnswer() {
-    int maxNumber = 10;
+long startProgramForAnswer() {
+    int maxNumber = 20;
 
-    // Keep track of a number until we've found the smallest answer
-    int currentNumber = maxNumber;
-    int answer = 0;
+    // Keep track of a number until we've found the smallest answer.
+    long currentNumber = maxNumber;
 
     // Loop until answer was found
-    while (answer == 0) {
-        bool allGoodSoFar = true;
-        // Check for every number from 1 to 20 if the current number is evenly divisible
+    while (1) {
+        // Check for every number from 1 to 20 if the current number is evenly divisible.
         for (int i = 1; i < maxNumber + 1; ++i) {
-            // If number is not evenly divisible, go to the next number
+            // If current number isn't divisible by i, there's no point in checking for the others, so break this loop.
             if (!isDivisible(currentNumber, i)) {
-                printf("Number %d is not evenly divisible by %d, moving on.\n", currentNumber, i);
-                allGoodSoFar = false;
-                currentNumber++;
-                continue;
+//                printf("%d is not divisible by %d, breaking loop.\n", currentNumber, i);
+                break;
             } else {
-                // Number is evenly divisible, check if we're at the last one and all's good so far
-                if (i == maxNumber && allGoodSoFar) {
-                    // All numbers are evenly divisible, answer found
-                    answer = currentNumber;
+//                printf("%d is divisible by %d, continuing...\n", currentNumber, i);
+                // Number is divisible, now check if we're at the last number and we'll have a winner!
+                if (i == maxNumber) {
+                    return currentNumber;
                 }
             }
         }
+        currentNumber++;
     }
-    return answer;
 }
 
 bool isDivisible(long numberToDivide, int numberToDivideWith) {
-    float x = numberToDivide;
-    float y = numberToDivideWith;
-    float z = x / y;
+    long double z = (long double) numberToDivide / numberToDivideWith;
 
-    if (floor(z) == z) {
+    if (floor((double) z) == z) {
         return true;
     }
     return false;
